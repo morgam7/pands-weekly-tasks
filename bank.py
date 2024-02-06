2c8302887cfa7cb9089ba3fc6b072494df9fd8b2
Marcella Morgan <contactmarcellamorgan@gmail.com>
2024-02-06 11:21:01 +0000

    bank

---
+++ bank.py
@@ -0,0 +1,8 @@
+# bank
+# program that inputs and adds up two integers
+# Author Marcella Morgan
+
+amount1 = int (input("Enter amount1(in cent): "))
+amount2 = int (input("Enter amount2(in cent): "))
+answer = float ((amount1 + amount2) /100)
+print ("The sum of these is ${}". format(answer))
