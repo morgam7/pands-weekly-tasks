85a4ed5cbb29877f2e221cd5e31e2c56c5640871
Marcella Morgan <contactmarcellamorgan@gmail.com>
2024-02-23 11:44:28 +0000

    Week 4 - collatz.py

--- collatz.py
+++ collatz.py
@@ -4,14 +4,20 @@
 # The program continues until the last value is one.
 # Author: Marcella Morgan
 
+numbers = []
 number = int (input ("Please enter a positive integer: "))   
 while number > 1:
+    numbers.append(number)
     if (number % 2) == 0:
-        newnumber = number / 2
+        newnumber = int (number / 2)
     else: 
-        newnumber = number * 3 + 1
-    print (int(newnumber))
+        newnumber = int (number * 3 + 1)
     number = newnumber
+for value in numbers:
+ print (value, end = " ")
+
+
+
 
 
 
@@ -21,11 +27,11 @@ while number > 1:
 # So first I need to do 'if elif else' to deal with the different calculations for odd and even numbers.
 # So the even numbers calculation:
 
-number = int (input ("Please enter a positive integer: "))
-if (number % 2) == 0:
-    print (int (number / 2))
-else: 
-    print ( "You have entered a very odd number.")
+#number = int (input ("Please enter a positive integer: "))
+#if (number % 2) == 0:
+#    print (int (number / 2))
+#else: 
+#    print ( "You have entered a very odd number.")
 
 # took ages to figure this out. Couldn't figure out why I needed the '== 0' or it won't understand the code. I didn't 
 # realise that this '(number % 2) == 0' is how you write an even number. I thought that you just needed to say if the
@@ -38,11 +44,11 @@ else:
 
 # Ok so now the odd numbers calculation:
 
-number = int (input ("Please enter a positive integer: "))
-if (number % 2) == 0:
-    print (int (number / 2))
-else: 
-    print (int (number * 3 + 1))   
+#number = int (input ("Please enter a positive integer: "))
+#if (number % 2) == 0:
+#    print (int (number / 2))
+#else: 
+#    print (int (number * 3 + 1))   
 
 # Ok so now the loop. 
 # I need to tell it to keep doing this until the number is 1.
@@ -56,31 +62,45 @@ else:
 
 # So many infinite loops! Had to figure out how to change the input so that it wouldn't keep looping:
         
-number = int (input ("Please enter a positive integer: "))   
-while number > 1:
-    if (number % 2) == 0:
-        newnumber = number / 2
-    else: 
-        newnumber = number * 3 + 1
-    print (int(newnumber))
-    number = newnumber
+#number = int (input ("Please enter a positive integer: "))   
+#while number > 1:
+#    if (number % 2) == 0:
+#        newnumber = number / 2
+#    else: 
+#        newnumber = number * 3 + 1
+#    print (int(newnumber))
+#    number = newnumber
 
 # So now can I figure out how to put them all on the same line or do we need to do this? If I write it as a list?
 
-numbers = []
-number = int (input ("Please enter a positive integer: "))   
-while number > 1:
-    numbers.append(number)
-    if (number % 2) == 0:
-        newnumber = number / 2
-    else: 
-        newnumber = number * 3 + 1
-    number = newnumber
-for value in numbers:
- print (int(value))
+#numbers = []
+#number = int (input ("Please enter a positive integer: "))   
+#while number > 1:
+#    numbers.append(number)
+#    if (number % 2) == 0:
+#        newnumber = number / 2
+#    else: 
+#        newnumber = number * 3 + 1
+#    number = newnumber
+#for value in numbers:
+# print (int(value))
 
 
 # Nope still on different lines. Will have to leave it there.
+
+# Saw post on slack channel that gave solution!
+ 
+#numbers = []
+#number = int (input ("Please enter a positive integer: "))   
+#while number > 1:
+#    numbers.append(number)
+#    if (number % 2) == 0:
+#        newnumber = int (number / 2)
+#    else: 
+#        newnumber = int (number * 3 + 1)
+#    number = newnumber
+#for value in numbers:
+# print (value, end = " ")
 
 
 
